70985e9297e4be94e853ab5e9a73d35b332cd0d7
devtom30 <devtom30@gmail.com>
2025-01-23 18:59:20 +0100

    simply redirect (hardcoded properties)

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use hyper::server::conn::AddrStream;
+use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::service::{service_fn, make_service_fn};
+use std::{convert::Infallible, net::SocketAddr};
+use std::net::IpAddr;
+
+fn debug_request(req: Request<Body>) -> Result<Response<Body>, Infallible>  {
+    let body_str = format!("{:?}", req);
+    Ok(Response::new(Body::from(body_str)))
+}
+
+async fn handle(client_ip: IpAddr, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match hyper_reverse_proxy::call(client_ip, "http://127.0.0.1:8084", req).await {
+        Ok(response) => {Ok(response)}
+        Err(_error) => {Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::empty())
+            .unwrap())}
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let bind_addr = "127.0.0.1:8000";
+    let addr:SocketAddr = bind_addr.parse().expect("Could not parse ip:port.");
+
+    let make_svc = make_service_fn(|conn: &AddrStream| {
+        let remote_addr = conn.remote_addr().ip();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| handle(remote_addr, req)))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Running server on {:?}", addr);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
